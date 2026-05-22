c96160f143c262d0ed7566aff5a7658e2ad76271
nads1983-coder <283250263+nads1983-coder@users.noreply.github.com>
2026-05-22 12:00:56 +0100

    Add password protection

---
+++ middleware.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server'
+
+export function middleware(req) {
+  const auth = req.headers.get('authorization')
+
+  if (auth) {
+    const encoded = auth.split(' ')[1]
+    const [user, pass] = atob(encoded).split(':')
+
+    if (
+      user === process.env.BASIC_AUTH_USER &&
+      pass === process.env.BASIC_AUTH_PASSWORD
+    ) {
+      return NextResponse.next()
+    }
+  }
+
+  return new Response('Login required', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  })
+}
+
+export const config = {
+  matcher: '/:path*',
+}
